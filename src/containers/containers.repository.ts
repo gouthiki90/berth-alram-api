db4158cc1846b83fddd4a7c9afb98cddfa61d3b9
somehow <snsdkf6@gmail.com>
2022-12-12 15:24:40 +0900

    [gaeun] 컨테이너 SELECT 시 반입 완료만 거르는 LIKE문 추가

--- src/containers/containers.repository.ts
+++ src/containers/containers.repository.ts
@@ -24,8 +24,9 @@ export class ContainersReposiotry {
         con.CNTR_STATUS,
         con.TERMINAL_NAME,
         con.STATUS_TM,
+        con.container_status,
         (SELECT COUNT(container_status) FROM container WHERE container_status = 1) AS finishCount,
-        (SELECT COUNT(id) FROM container WHERE berth_oid = berth.oid) AS conCount
+        (SELECT COUNT(id) FROM container WHERE berth_oid = berth.oid AND con.STATUS_NM LIKE '%반입%') AS conCount
       FROM container AS con
       LEFT JOIN berthStat_schedule AS berth ON con.berth_oid = berth.oid
       WHERE TRUE
@@ -40,6 +41,7 @@ export class ContainersReposiotry {
   async findOne(query: any) {
     const { berthOid, alramOid } = query;
     const whereArr = [
+      ["AND con.STATUS_NM LIKE '%반입%'", true],
       ["AND berth.oid = :berthOid", berthOid],
       ["AND con.alram_oid = :alramOid", alramOid],
     ];
@@ -56,8 +58,9 @@ export class ContainersReposiotry {
       con.CNTR_STATUS,
       con.TERMINAL_NAME,
       con.STATUS_TM,
+      con.container_status,
       (SELECT COUNT(container_status) FROM container WHERE container_status = 1) AS finishCount,
-      (SELECT COUNT(id) FROM container WHERE berth_oid = berth.oid) AS conCount
+      (SELECT COUNT(id) FROM container WHERE berth_oid = berth.oid AND con.STATUS_NM LIKE '%반입%') AS conCount
     FROM container AS con
     LEFT JOIN berthStat_schedule AS berth ON con.berth_oid = berth.oid
     WHERE TRUE
