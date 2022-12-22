45248f3fed4e71d7736ea9f1dcd8ebfc9fe318c2
somehow <snsdkf6@gmail.com>
2022-12-22 17:00:30 +0900

    [gaeun] 신항/북항 관련 berth-py에서 로직 삭제, is change 로거 warn으로 변경
    다른 테이블 생성으로 LEFT JOIN했기 때문에 관련 로직은 삭제함

--- src/berth-py/berth-py.service.ts
+++ src/berth-py/berth-py.service.ts
@@ -107,16 +107,6 @@ export class BerthPyService {
 
   async create(data: Array<CreateBerthPyDto>) {
     const t = await this.seqeulize.transaction();
-    /** 신항 터미널 리스트 */
-    const NEW_PORT_LIST = [
-      "PNIT",
-      "PNC",
-      "HJNC",
-      "HPNT",
-      "BNCT",
-      "UNCT",
-      "PICT",
-    ];
     try {
       for (const obj of data) {
         // duple check
@@ -126,20 +116,13 @@ export class BerthPyService {
         const userInfoList = await this.findUserInfoListForAlram(obj);
 
         if (berthDupleData) {
-          /** 신항은 1로 대입 */
-          NEW_PORT_LIST.forEach((element: string) => {
-            if (element === obj.trminlCode) {
-              obj.isNewPort = 1;
-            }
-          });
-
           await berthStatSchedule.update(obj, {
             where: { oid: obj.oid },
             transaction: t,
           });
 
           if (berthDupleData.csdhpPrarnde !== obj.csdhpPrarnde) {
-            console.log(`csdhpPrarnde=${obj.csdhpPrarnde} ::: is change! :::`);
+            Logger.warn(`csdhpPrarnde=${obj.csdhpPrarnde} ::: is change! :::`);
 
             /** 이전 접안일 데이터 update */
             await berthStatSchedule.update(
