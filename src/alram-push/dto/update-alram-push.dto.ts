4b58417a341a1597ffd3a8ae16b41077620f8d27
somehow <snsdkf6@gmail.com>
2023-01-03 16:47:43 +0900

    [gaeun] 사용하지 않는 dto 파일 삭제

--- src/alram-push/dto/update-alram-push.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateAlramPushDto } from "./create-alram-push.dto";
-
-export class UpdateAlramPushDto extends PartialType(CreateAlramPushDto) {}
