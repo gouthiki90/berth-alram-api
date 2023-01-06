8a6a2429e445ed8f71f7d8813a5c4c384b0fb030
somehow <snsdkf6@gmail.com>
2023-01-06 15:59:16 +0900

    [gaeun] 컨테이너 카운트 쿼리 수정

--- src/containers/containers.repository.ts
+++ src/containers/containers.repository.ts
@@ -31,7 +31,7 @@ export class ContainersReposiotry {
       con.STATUS_TM,
       con.container_status,
       (SELECT COUNT(B.cnt) FROM (SELECT COUNT(container.oid) AS cnt FROM container WHERE TRUE AND berth_oid = '${berthOid}' AND alram_oid = '${alramOid}' GROUP BY CNTR_NO ORDER BY STATUS_DT, STATUS_TM DESC) AS B) AS conCount,
-      (SELECT COUNT(A.cnt) FROM (SELECT COUNT(container.container_status) AS cnt from container WHERE TRUE AND container_status = 1 AND berth_oid = '${berthOid}' AND alram_oid = '${alramOid}' GROUP BY CNTR_NO ORDER BY STATUS_DT, STATUS_TM DESC) AS A) AS finishCount
+      (SELECT COUNT(A.cnt) FROM (SELECT COUNT(container.container_status) AS cnt from container WHERE TRUE AND berth_oid = '${berthOid}' AND alram_oid = '${alramOid}' GROUP BY CNTR_NO ORDER BY STATUS_DT, STATUS_TM DESC) AS A) AS finishCount
     FROM container AS con
     LEFT JOIN berthStat_schedule AS berth ON con.berth_oid = berth.oid
     WHERE TRUE
@@ -67,7 +67,7 @@ export class ContainersReposiotry {
       con.STATUS_TM,
       con.container_status,
       (SELECT COUNT(B.cnt) FROM (SELECT COUNT(container.oid) AS cnt FROM container WHERE TRUE AND berth_oid = '${berthOid}' AND alram_oid = '${alramOid}' GROUP BY CNTR_NO ORDER BY STATUS_DT DESC) AS B) AS conCount,
-      (SELECT COUNT(A.cnt) FROM (SELECT COUNT(container.container_status) AS cnt from container WHERE TRUE AND container_status = 1 AND berth_oid = '${berthOid}' AND alram_oid = '${alramOid}' GROUP BY CNTR_NO ORDER BY STATUS_DT DESC) AS A) AS finishCount
+      (SELECT COUNT(A.cnt) FROM (SELECT COUNT(container.container_status) AS cnt from container WHERE TRUE AND berth_oid = '${berthOid}' AND alram_oid = '${alramOid}' GROUP BY CNTR_NO ORDER BY STATUS_DT DESC) AS A) AS finishCount
     FROM container AS con
     LEFT JOIN berthStat_schedule AS berth ON con.berth_oid = berth.oid
     WHERE TRUE
