4e11966ddb1c7ec32fd666cf1aca7c8586a0b197
somehow <snsdkf6@gmail.com>
2023-01-26 11:02:48 +0900

    [gaeun] dto에 주석 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,22 @@
 export class CreateUserDto {
-  id?: number;
+  /** 유저 키값 */
   oid?: string;
+  /** 유저 아이디 */
   userId: string;
+  /** 패스워드 */
+  password?: any;
+  /** 유저 이름 */
   userName?: string;
-  password: any;
-  bizName?: string;
+  /** 상호 */
+  bizName: string;
+  /** 전화번호 */
   contact?: string;
+  /** 담당자 전화번호 */
   managerTel?: string;
+  /** 담당자 이름 */
   managerName?: string;
+  /** 카톡/문자 옵션 */
   contact_option?: number;
+  /** 문자 on-off 옵션 */
+  isNofitication?: number;
 }
--- src/user/dto/login.dto.ts
+++ src/user/dto/login.dto.ts
@@ -1,4 +1,6 @@
 export class LoginDto {
+  /** 유저 아이디 */
   userId: string;
+  /** 비밀번호 */
   password: any;
 }
