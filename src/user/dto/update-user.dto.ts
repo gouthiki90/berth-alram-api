dc59a43a54af14e8dddcff5e33882245c5f5f0fb
somehow <snsdkf6@gmail.com>
2023-02-02 13:47:48 +0900

    [gaeun] dto에 추가

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -12,6 +12,8 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
   bizName: string;
   /** 전화번호 */
   contact?: string;
+  /** 연락처 2 */
+  contact_01?: string;
   /** 담당자 전화번호 */
   managerTel?: string;
   /** 담당자 이름 */
