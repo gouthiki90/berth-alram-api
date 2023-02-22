c8cab3da7aa6a5954b33b6bbcb50c5129fc463d8
somehow <snsdkf6@gmail.com>
2023-02-22 10:18:01 +0900

    [gaeun] repository controller 단에 적용

--- src/system/management/management.controller.ts
+++ src/system/management/management.controller.ts
@@ -10,24 +10,33 @@ import {
 import { ManagementService } from "./management.service";
 import { CreateManagementDto } from "./dto/create-management.dto";
 import { UpdateManagementDto } from "./dto/update-management.dto";
+import { ManagementRepository } from "./management.repository";
 
 @Controller("management")
 export class ManagementController {
-  constructor(private readonly managementService: ManagementService) {}
+  constructor(
+    private readonly managementService: ManagementService,
+    private readonly managementRepository: ManagementRepository
+  ) {}
 
   @Post()
   create(@Body() createManagementDto: CreateManagementDto) {
     return this.managementService.create(createManagementDto);
   }
 
-  @Get()
-  findAll() {
-    return this.managementService.findAll();
+  @Get("/")
+  findAllUserInfoForSuper() {
+    return this.managementRepository.findAllUserInfoForSuper();
   }
 
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.managementService.findOne(+id);
+  @Get("/user")
+  findOneUserInfoForSuper(@Param("oid") oid: string) {
+    return this.managementRepository.findOneUserInfoForSuper(oid);
+  }
+
+  @Get("/company")
+  findAllCompanyInfoForSuper() {
+    return this.managementRepository.findAllCompanyInfoForSuper();
   }
 
   @Patch(":id")
