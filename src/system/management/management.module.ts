80286ad513b04f0870137003ca010eceac0eb03c
somehow <snsdkf6@gmail.com>
2023-03-31 12:55:13 +0900

    [gaeun] 모델 인스턴스 오류 수정

--- src/system/management/management.module.ts
+++ src/system/management/management.module.ts
@@ -2,12 +2,12 @@ import { Module } from "@nestjs/common";
 import { ManagementService } from "./management.service";
 import { ManagementController } from "./management.controller";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { commonCode, user } from "src/models";
+import { commonCode, stmCompany, user } from "src/models";
 import { ManagementRepository } from "./management.repository";
 import { Utils } from "src/util/common.utils";
 
 @Module({
-  imports: [SequelizeModule.forFeature([user, commonCode])],
+  imports: [SequelizeModule.forFeature([user, commonCode, stmCompany])],
   controllers: [ManagementController],
   providers: [ManagementService, ManagementRepository, Utils],
 })
