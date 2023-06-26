d15cfc21c0730e28bf680e95410e81db5b76ca4f
somehow <snsdkf6@gmail.com>
2023-06-26 11:04:57 +0900

    Fix : SSL 해제

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import * as bodyParser from "body-parser";
 import * as fs from "fs";
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`, "utf8"),
-    cert: fs.readFileSync(`${process.env.SSL_CRT_PATH}`, "utf8"),
-  };
+  // const httpsOptions = {
+  //   key: fs.readFileSync(`${process.env.SSL_KEY_PATH}`, "utf8"),
+  //   cert: fs.readFileSync(`${process.env.SSL_CRT_PATH}`, "utf8"),
+  // };
 
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   // the next two lines did the trick
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
